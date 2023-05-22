f38370b8436d310f250496dcfc20d1434fb578a8
Antonio Gennaro <antgennaro@hotmail.com>
2023-05-22 16:32:47 -0300

    Vectores Ej. 1 Parte #1 y #2

---
+++ vectores_ej1.js
@@ -0,0 +1,24 @@
+/*
+const numeros   = [2,7,9,8,3,1,0];
+
+console.log(numeros[0]+", "+numeros[3]+", "+numeros[6]);
+console.log(numeros[0]);
+console.log(numeros[3]);
+console.log(numeros[6]);
+
+console.log(numeros[1]+" Es el 2do número del vector 'numeros'");
+*/
+
+
+const numeros1   = [2, 7, 9, 8, 3, 1, 0];  
+const letras     = ["a", "b", "c", "d", "e", "f", "g"];
+const semana     = ["lun", "mar", "mie", "jue", "vie", "sab", "dom"];   
+
+console.log(numeros1[0]+", "+numeros1[3]+", "+numeros1[6]);
+console.log(letras[0]+", "+letras[3]+", "+letras[6]);
+console.log(semana[0]+", "+semana[3]+", "+semana[6]);
+
+console.log(numeros1[1]+" Es el 2do número del vector 'numeros', "+
+            letras[2]+" Es la tercera letra del vector 'letras', "+
+            semana[3]+" Es la cuarta cadena del vector 'semana'");
+            
